2d892a9be1f7b483f59d8162f1fd122a39ac9313
Khayyam Ahmed <82821091+khayyam-ahmed@users.noreply.github.com>
2023-05-08 05:41:23 +0500

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -1,5 +1,10 @@
 
-pip3 install -r requirements.txt
+apt-get update && apt-get install -y libgl1-mesa-glx
+apt-get install -y libglib2.0-0
+
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
+pip install -r requirements.txt
 
 
 
@@ -11,14 +16,21 @@ chown root:root /swapfile
 chmod 0600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-pip3 --no-cache-dir  install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip --no-cache-dir  install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 
-pip3 --no-cache-dir install ultralytics
-pip3 install tensorflow-cpu
-pip3 install asyncio
+pip --no-cache-dir install ultralytics
 
-pip3 uninstall -y opencv-contrib-python
-pip3 uninstall -y opencv-python
 
-pip3 install --force-reinstall opencv-python-headless==4.5.4.58
+export CUDA_VISIBLE_DEVICES=''
+export TF_ENABLE_TENSORRT=0
+export TF_DISABLE_DEPRECATION_WARNINGS=1
+export TF_ENABLE_ONEDNN_OPTS=0
+
+
+pip install tensorflow-cpu
+
+pip uninstall -y opencv-contrib-python
+pip uninstall -y opencv-python
+
+pip install --force-reinstall opencv-python-headless==4.5.4.58
