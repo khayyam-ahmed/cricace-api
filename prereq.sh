b345deb3e302eaef7a7a58af80846b94dd393d2f
Khayyam Ahmed <82821091+khayyam-ahmed@users.noreply.github.com>
2023-05-09 20:23:18 +0500

    updated __pycache__

--- prereq.sh
+++ prereq.sh
@@ -66,4 +66,4 @@ mkdir "model\bowlingTypeClassificationModels\content"
 
 pip3 install --force-reinstall opencv-python-headless==4.5.4.58
 
-gunicorn -w  4 -k uvicorn.workers.UvicornWorker main:app
+# gunicorn -w  4 -k uvicorn.workers.UvicornWorker main:app
